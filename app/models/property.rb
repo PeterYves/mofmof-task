502f2d1cd89ef86100cbb35a06d97eb3b262d8db
MUJAWAMARIYAbundle <iradupeteryves@gmail.com>
2019-11-26 09:08:45 +0200

    show nearest rail way 1&2 information not mixing them

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,6 @@
 class Property < ApplicationRecord
     validate :check
+    validates :building_age,:how_many_minutes_walk,:how_many_minute_walk, numericality: true
     def check
         if how_many_minutes_walk <= how_many_minute_walk
             errors.add(:how_many_minutes_walk, 'please check again')

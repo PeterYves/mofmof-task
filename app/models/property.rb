b0adcf88050e4aad124e410d8d711c19bd6ec3f5
MUJAWAMARIYAbundle <mumarg2015@gmail.com>
2019-12-05 16:27:39 +0200

    correcting mofmof acccording to final requirements

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,13 +1,14 @@
 class Property < ApplicationRecord
-    has_many :stations
-    validates :property, :rent, :address,:building_age, presence: true
-    validates :rent, numericality: true   
-    accepts_nested_attributes_for :stations, reject_if: :all_blank, :allow_destroy => true
-        def check
-            if how_many_minutes_walks.present? && how_many_minutes_walks1.present?
-                if how_many_minutes_walks <= how_many_minutes_walks1
-                    errors.add(:how_many_minutes_walks, 'please check again')
-                end
-            end
-        end
+  has_many :stations
+  validates :property, :rent, :address,:building_age, presence: true
+  validates :rent, numericality: true   
+  accepts_nested_attributes_for :stations, reject_if: :all_blank, :allow_destroy => true
+  
+  def check
+    if how_many_minutes_walks.present? && how_many_minutes_walks1.present?
+      if how_many_minutes_walks <= how_many_minutes_walks1
+        errors.add(:how_many_minutes_walks, 'please check again')
+      end
+    end
+  end
 end
